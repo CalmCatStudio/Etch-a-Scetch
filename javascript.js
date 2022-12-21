3a8f4212f5ac99ab0452cc84c974f8ad37524fcf
Kris Anderson <CalmCatStudio@gmail.com>
2022-12-21 00:32:51 -0500

    Add Color Picker & Size Control

--- javascript.js
+++ javascript.js
@@ -1,15 +1,40 @@
+let isMouseDown = false;
+
+let color = 'black';
+let colorInput = document.getElementById('color');
+colorInput.addEventListener("input", function() {
+    color = colorInput.value;
+})
+
 let gridSize = 16;
 const container = document.querySelector('.container');
-let isMouseDown = false;
+container.addEventListener("mouseleave", function() {
+    isMouseDown = false;
+})
+
+const sizeInput = document.getElementById('size');
+const sizeLabel = document.getElementById('sizeLabel');
+sizeInput.addEventListener("change", function(){
+    updateGrid(sizeInput.value);
+})
+
 createGrid();
 
 function changeColor(box)
 {
-    box.style.backgroundColor = 'black';
+    box.style.backgroundColor = color;
+}
+
+function updateGrid(newSize)
+{
+    gridSize = sizeInput.value;
+    sizeLabel.textContent = `Size: ${gridSize} X ${gridSize}`;
+    createGrid();
 }
 
 function createGrid()
 {
+    container.innerHTML = '';
     for (let y = 0; y < gridSize; y++)
     {
         const row = document.createElement('div');
@@ -32,10 +57,7 @@ function createGrid()
                     changeColor(box);
                 }
             });
-            box.addEventListener('click', function()
-            {
-                box.style.backgroundColor = 'black';
-            });
+            box.addEventListener
 
             row.appendChild(box);
         }
